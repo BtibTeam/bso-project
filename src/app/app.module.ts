77207999203f3664c6832f35b7af0ac062acc831
Olivier <olivier.poumeyrol@gmail.com>
2018-01-13 11:50:54 +0100

    Integration of Firebase SDK, AngularFire2 library and ClassTransformer library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+// Firestore
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 // Pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +15,17 @@ import { HomePage } from '../pages/home/home';
 // Providers
 import { NodeHandlerProvider } from '../providers/node-handler/node-handler';
 import { NodeDataProvider } from '../providers/node-data/node-data';
+import { FirestoreProvider } from '../providers/firestore/firestore';
+
+// Firestore config
+const config = {
+  apiKey: "AIzaSyDLBB_EB-KcPswmnvblVtbrZvM0PPF1E8k",
+  authDomain: "hso-project.firebaseapp.com",
+  databaseURL: "https://hso-project.firebaseio.com",
+  projectId: "hso-project",
+  storageBucket: "hso-project.appspot.com",
+  messagingSenderId: "856263060419"
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +34,10 @@ import { NodeDataProvider } from '../providers/node-data/node-data';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // enablePersistence(): Offline data
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +47,8 @@ import { NodeDataProvider } from '../providers/node-data/node-data';
   providers: [
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     NodeHandlerProvider,
-    NodeDataProvider
+    NodeDataProvider,
+    FirestoreProvider
   ]
 })
 export class AppModule { }
