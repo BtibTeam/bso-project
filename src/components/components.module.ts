793df9d37016737fbc38861ac540fd747c474d38
Olivier <olivier.poumeyrol@gmail.com>
2018-02-02 16:59:58 +0100

    Introduction of the Addons Editor component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,14 +1,44 @@
 // Framework
 import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+
+// Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 // Components
 import { NodeEditor } from './node-editor/node-editor';
+import { AddonsManager } from './addons-manager/addons-manager';
+import { AddonsEditor } from './addons-editor/addons-editor';
+
+// Directives
+import { DirectivesModule } from '../directives/directives.module';
 
 @NgModule({
 	declarations: [
-		NodeEditor],
-	imports: [],
+		NodeEditor,
+		AddonsManager,
+		AddonsEditor
+	],
+	imports: [
+		IonicModule,
+		DirectivesModule,
+		MatFormFieldModule,
+		MatTableModule,
+		MatInputModule,
+		MatDialogModule,
+		MatCheckboxModule
+	],
+	entryComponents: [
+		AddonsEditor,
+	],
 	exports: [
-		NodeEditor]
+		NodeEditor,
+		AddonsManager,
+		AddonsEditor
+	]
 })
 export class ComponentsModule { }
