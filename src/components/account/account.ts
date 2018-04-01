34f3d351474285b68768d9c34b683eefdbd29ba4
Olivier <olivier.poumeyrol@gmail.com>
2018-04-01 18:11:37 +0200

    Creation of the Account component to handle signIn and signOut

---
+++ src/components/account/account.ts
@@ -0,0 +1,57 @@
+// Angular
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+
+// Firestore
+import User from 'firebase/auth';
+
+// Provider
+import { FireAuthProvider } from '../../providers/fire-auth/fire-auth';
+
+@Component({
+  selector: 'account',
+  templateUrl: 'account.html'
+})
+export class accountComponent {
+
+  // Inputs
+  @Input('user') user: User = null;
+
+  // Outputs
+  @Output() private loggedIn: EventEmitter<void> = new EventEmitter<void>();
+
+  private loginForm: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authPvd: FireAuthProvider) {
+
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.compose([Validators.minLength(2), Validators.required])],
+    });
+
+  }
+
+  ////////////////////////////////////////////////////////////////
+  // User Interactions
+  ////////////////////////////////////////////////////////////////
+
+  /**
+   * SignIn the user to the Database
+   */
+  protected signIn(): void {
+    this.authPvd.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password).then(() => {
+      this.loggedIn.emit();
+    });
+  }
+
+
+  /**
+   * Sign out the user to the Database
+   */
+  protected signOut(): void {
+    this.authPvd.signOut();
+  }
+
+}
